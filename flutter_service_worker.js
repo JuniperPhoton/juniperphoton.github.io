4d3038329621c13285a333af96dd5d57ced3caac
juniperphoton <dengweichao@hotmail.com>
2022-12-10 23:15:04 +0800

    Drives me crazy!

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "50058bcceebe247db1e3ee42ff548925",
-"index.html": "cdaf67045c7b7fef9167ef8ad9094213",
-"/": "cdaf67045c7b7fef9167ef8ad9094213",
-"main.dart.js": "c0e044d8d11675bbf6d37fae196ef09d",
+"index.html": "97508e41ce76b0df5aa47fc0f034dc31",
+"/": "97508e41ce76b0df5aa47fc0f034dc31",
+"main.dart.js": "699b2618388d2d90d7a5e727af2fee56",
+"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -18,7 +19,11 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/shaders/ink_sparkle.frag": "bf917379b2113d7e92955e3dbbafcd57",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b"
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62"
 };
 
 // The application shell files that are downloaded before a service worker can
